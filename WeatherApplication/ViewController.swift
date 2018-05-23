8661e135ca3759ca5f11045db37ce0206ce4fd5c
HamadMouhamed <llovesilkroad39@gmail.com>
2018-05-23 15:05:24 +0200

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Hamada on 5/23/18.
+//  Copyright © 2018 Hamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
