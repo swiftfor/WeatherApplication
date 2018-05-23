ec3caa95384d7b3f662115b18568ecf31cc03f02
HamadMouhamed <llovesilkroad39@gmail.com>
2018-05-23 23:45:11 +0200

    WeatherApplication

--- WeatherApplication/ViewController.swift
+++ WeatherApplication/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var laDisplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
+
+   
+    @IBAction func getWeatherAction(_ sender: Any) {
+        if let  url = URL(string: "https://www.weather-forecast.com/locations/"+cityTextField.text!.replacingOccurrences(of: "", with: "-")+"/forecasts/latest"){
+            let request = NSMutableURLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+                var message = ""
+                if error != nil{
+                    print(error)
+                }else{
+                    if let unwrappeddata = data{
+                        let dataString = NSString(data: unwrappeddata, encoding: String.Encoding.utf8.rawValue)
+                        var stringseparator = "<span class=\"phrase\">"
+                        if let contentArray = dataString?.components(separatedBy: stringseparator){
+                            if contentArray.count > 1{
+                                stringseparator = "</span>"
+                                let newcontentArray = contentArray[1].components(separatedBy: stringseparator)
+                                if newcontentArray.count > 1{
+                                    message = newcontentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                    print(message)
+                                }
+                                
+                            }
+                        }
+                    }
+                    
+               
+                }
+                if message == "" {
+                    message = "the weather there cannot be found , please try again later..."
+                }
+                DispatchQueue.main.sync(execute: {
+                    self.laDisplay.text = message
+                })
+            }
+            task.resume()
+        }else{
+            laDisplay.text = "the weather there cannot be found , please try again later..."
+        }
+}
+    
 
 }
+
 
